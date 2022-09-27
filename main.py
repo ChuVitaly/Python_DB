c7e791928af891a9b123334b6890c1231ba93e71
Vitaly <chuvitg@gmail.com>
2022-09-27 14:11:52 +0300

    CREATE function add_new_customer

--- main.py
+++ main.py
@@ -11,22 +11,34 @@ with conn.cursor() as cur:
     # Создание таблицы
     def create_tables():
         cur.execute("""
-        CREATE TABLE IF NOT EXISTS customers(
-            CustomerId SERIAL PRIMARY KEY,
-            Name VARCHAR(40),
-            Lastname VARCHAR(60) NOT NULL,
-            Email VARCHAR(40) NOT NULL
-        );
-        """)
+            CREATE TABLE IF NOT EXISTS customers(
+                CustomerId SERIAL PRIMARY KEY,
+                Name VARCHAR(40),
+                LastName VARCHAR(60) NOT NULL,
+                Email VARCHAR(40) NOT NULL
+            );
+            """)
         cur.execute("""
-        CREATE TABLE IF NOT EXISTS phone(
-            PhoneId PRIMARY KEY,
-            CustomerId INT FOREIGN KEY REFERENCES customers(CustomerId),
-            PhoneNumber VARCHAR(50)
-        );
-        """)
+            CREATE TABLE IF NOT EXISTS phone(
+                PhoneId SERIAL PRIMARY KEY,
+                CustomerId INT REFERENCES customers(CustomerId),
+                PhoneNumber VARCHAR(50)
+            );
+            """)
+        conn.commit()
 
+    # Заполняем таблицу
+    def add_new_customer(cur, Name, LastName, Email, PhoneNumber):
+        cur.execute("""
+            INSERT INTO customers(Name, LastName, Email) VALUES(%s, %s, %s);
+            """, (Name, LastName, Email))
 
-    # conn.commit()
+        cur.execute("""
+            INSERT INTO phone(PhoneNumber) VALUES(%s);
+        """, (PhoneNumber,))
+        conn.commit()
+
+    # create_tables()
+    # add_new_customer(cur, 'Nick', 'Mull', 'g12131@mail.ru', '8921337859')
 
 conn.close()
